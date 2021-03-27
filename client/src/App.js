3eb6876f2378bda895d938aef1090cdf283aca49
Doki064 <52833936+Doki064@users.noreply.github.com>
2021-03-27 17:54:01 +0700

    Update index.js client

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import { Navbar, Nav, Form, FormControl, Button } from "react-bootstrap";
-import { BrowserRouter, HashRouter, Route, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 import HomePage from "./components/HomePage";
 import Dashboard from "./components/Dashboard";
@@ -13,68 +13,64 @@ function App() {
   if (sessionStorage.getItem("session")) {
     return (
         <div className="App">
-          <HashRouter basename={process.env.PUBLIC_URL}>
-            <Navbar bg="light" expand="lg">
-              <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
-              <Navbar.Toggle aria-controls="basic-navbar-nav" />
-              <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
-                  <Nav.Link href={"/"}>Home</Nav.Link>
-                  <Nav.Link href={`/dashboard`}>Dashboard</Nav.Link>
-                </Nav>
-                <Form inline>
-                  <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                  <Button variant="outline-success">Search</Button>
-                </Form>
-              </Navbar.Collapse>
-            </Navbar>
+          <Navbar bg="light" expand="lg">
+            <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mr-auto">
+                <Nav.Link href={"/"}>Home</Nav.Link>
+                <Nav.Link href={`/dashboard`}>Dashboard</Nav.Link>
+              </Nav>
+              <Form inline>
+                <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                <Button variant="outline-success">Search</Button>
+              </Form>
+            </Navbar.Collapse>
+          </Navbar>
 
-            <Switch>
-              <Route exact path={"/"}>
-                <HomePage />
-              </Route>
-              <Route path={"/login"}>
-                <LoginForm />
-              </Route>
-              <Route path={"/dashboard"}>
-                <Dashboard />
-              </Route>
-            </Switch>
-          </HashRouter>
+          <Switch>
+            <Route exact path={"/"}>
+              <HomePage />
+            </Route>
+            <Route exact path={"/login"}>
+              <LoginForm />
+            </Route>
+            <Route exact path={"/dashboard"}>
+              <Dashboard />
+            </Route>
+          </Switch>
         </div>
     )
   }
   return (
     <div className="App">
-      <BrowserRouter>
-        <Navbar bg="light" expand="lg">
-          <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto">
-              <Nav.Link href={"/"}>Home</Nav.Link>
-              <Nav.Link href={"/login"}>Log In</Nav.Link>
-              <Nav.Link href={"/register"}>Sign Up</Nav.Link>
-            </Nav>
-            <Form inline>
-              <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-              <Button variant="outline-success">Search</Button>
-            </Form>
-          </Navbar.Collapse>
-        </Navbar>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href={"/"}>Home</Nav.Link>
+            <Nav.Link href={"/login"}>Log In</Nav.Link>
+            <Nav.Link href={"/register"}>Sign Up</Nav.Link>
+          </Nav>
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Navbar>
 
-        <Switch>
-          <Route exact path={"/"}>
-            <HomePage />
-          </Route>
-          <Route path={"/login"}>
-            <LoginForm />
-          </Route>
-          <Route path={"/register"}>
-            <RegisterForm />
-          </Route>
-        </Switch>
-      </BrowserRouter>
+      <Switch>
+        <Route exact path={"/"}>
+          <HomePage />
+        </Route>
+        <Route exact path={"/login"}>
+          <LoginForm />
+        </Route>
+        <Route exact path={"/register"}>
+          <RegisterForm />
+        </Route>
+      </Switch>
     </div>
   );
 }
